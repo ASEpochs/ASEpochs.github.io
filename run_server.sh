d625687f58c1904ec946a3abde2c00e209d076b8
ASEpochs <2536409884@qq.com>
2026-03-21 20:35:19 +0800

    update

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,4 @@
-bundle exec jekyll build
-cd ~/githubdemo/1个人主页创建/ASEpochs.github.io-main/_site
-python3 -m http.server 4000
+# bundle exec jekyll build
+# cd ~/githubdemo/1个人主页创建/ASEpochs.github.io-main/_site
+# python3 -m http.server 4000
+bundle exec jekyll liveserve
